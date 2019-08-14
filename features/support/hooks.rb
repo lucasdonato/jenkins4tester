a9f76355bf9dc49d8dcfedf79886c0f25ef93365
=lucasdonato <=lucaspolimig96@gmail.com>
2019-08-14 12:02:43 -0300

    add report builder

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,3 +1,5 @@
+require "report_builder"
+
 Before do
   @login_page = LoginPage.new
   @movie_page = MoviePage.new
@@ -16,4 +18,22 @@ end
 After do |scenario|
   screenshot = page.save_screenshot("log/screenshots/#{scenario.__id__}.png")
   embed(screenshot, "image/png", "Screenshot")
+end
+
+at_exit do
+  @infos = {
+    "Autor" => "Lucas Donato",
+    "environment" => "Dev",
+    "Data do Teste" => Time.now.to_s,
+  }
+
+  ReportBuilder.configure do |config|
+    config.input_path = "log/report.json"
+    config.report_path = "log/report"
+    config.report_types = [:html]
+    config.report_title = "Jenkins4testers"
+    config.additional_info = @infos
+    config.color = "indigo"
+  end
+  ReportBuilder.build_report
 end
